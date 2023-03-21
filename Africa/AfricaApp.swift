826ec90c0baa66bdf93b6fcabf7c035a7a712518
Jonathan <7716662+jschaffer001@users.noreply.github.com>
2023-03-21 15:38:29 -0700

    Initial Commit

---
+++ Africa/AfricaApp.swift
@@ -0,0 +1,17 @@
+//
+//  AfricaApp.swift
+//  Africa
+//
+//  Created by Jonathan Schaffer on 3/21/23.
+//
+
+import SwiftUI
+
+@main
+struct AfricaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
