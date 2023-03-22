aab3e21b13613ae620215e4650f5b2bc4d82d002
Jonathan <7716662+jschaffer001@users.noreply.github.com>
2023-03-22 11:37:17 -0700

    Completed the animal detail view buildout with the addition of numerous subviews each with their own custom layout for content like gallery photos, horizontally scrollable facts, description, location and external link to additional information from a reputable source.

--- Africa/App/ContentView.swift
+++ Africa/App/ContentView.swift
@@ -9,7 +9,7 @@ import SwiftUI
 
 struct ContentView: View {
     // MARK: - PROPERTIES
-    
+    let animals: [Animal] = Bundle.main.decode("animals.json")
     
     // MARK: - BODY
     var body: some View {
@@ -18,6 +18,12 @@ struct ContentView: View {
                 CoverImageView()
                     .frame(height: 300)
                     .listRowInsets(EdgeInsets(top: 0, leading: 0, bottom: 0, trailing: 0))
+                
+                ForEach(animals) { animal in
+                    NavigationLink(destination: AnimalDetailView(animal: animal)) {
+                        AnimalListItemView(animal: animal)
+                    } //: LINK
+                } //: LOOP
             } //: LIST
             .navigationBarTitle("Africa", displayMode: .large)
         } //: NAVIGATION
