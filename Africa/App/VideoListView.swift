b5e4fcaca122b625620460842df9e425783e66cb
Jonathan <7716662+jschaffer001@users.noreply.github.com>
2023-03-21 17:46:13 -0700

    Prepped the project by adding all of the different main views and then created a tab view in the main view to quickly switch between all of the views that were just created.

---
+++ Africa/App/VideoListView.swift
@@ -0,0 +1,20 @@
+//
+//  VideoListView.swift
+//  Africa
+//
+//  Created by Jonathan Schaffer on 3/21/23.
+//
+
+import SwiftUI
+
+struct VideoListView: View {
+    var body: some View {
+        Text("Video")
+    }
+}
+
+struct VideoListView_Previews: PreviewProvider {
+    static var previews: some View {
+        VideoListView()
+    }
+}
